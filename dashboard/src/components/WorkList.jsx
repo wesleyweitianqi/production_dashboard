e1c5a9f69ea8033dae1a6ddb5261889295f55aac
wesleyweitianqi <wesleytianqiwei@gmail.com>
2022-12-30 11:29:28 -0500

    worklist update

--- dashboard/src/components/WorkList.jsx
+++ dashboard/src/components/WorkList.jsx
@@ -1,14 +1,34 @@
-import React from "react";
-import Order from "./Order";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+
 
 const WorkList = () => {
-  
+  const [data, setData] =useState([])
+  useEffect(()=> {
+    axios.get("http://localhost:3000/data").then((res) => {
+      setData(res.data);
+    });
+  }, [data])
+   const dataEle = data.map((item) => {
+    return (
+      <div
+      key={item.wo}
+        className="card border-primary mb-3"
+        style={{ maxWidth: "18rem", margin: "1rem auto" }}
+      >
+        <div className="card-header">{item.wo}</div>
+        <div className="card-body text-primary">
+          <h5 className="card-title">{item.catalog}</h5>
+          <p className="card-text">{item.description}</p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col-md-8">
-          <Order />
-        </div>
+        <div className="col-md-8">{data ? dataEle : "isLoading"}</div>
       </div>
     </div>
   );
